c5bb780797c714c8f824cf48db3617fa7a0ed1e2
thomas-um-grant <131417866+thomas-um-grant@users.noreply.github.com>
2025-05-14 22:45:28 +0100

    practice on evals

---
+++ notebooks/practice/evaluation/evaluate.sh
@@ -0,0 +1,6 @@
+vidore-benchmark evaluate-retriever \
+    --model-class colpali \
+    --model-name vidore/colpali-v1.3 \
+    --collection-name vidore/ \
+    --dataset-format qa \
+    --split test
